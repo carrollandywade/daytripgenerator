dde352d74ee0c7c32949b1d93c27081896a0f8a6
Andy Wade <andyandamandawade@hotmail.com>
2021-05-28 14:11:15 -0500

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Day Trip Generator
+
+let destinations = ['Beach', 'Museum', 'Park', 'Finishing'];
+let Restaurants = ['McDonals', 'Chick-fil-A', 'Taco Bell', 'Caffeteria'];
+let modeOfTransportation = ['Bus', 'Bicycle', 'Rental Car', 'Taxi'];
+let Entertainment = ['Swimming', 'Theater', 'Shopping', 'Dancing'];
